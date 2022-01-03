885af918ecb0ed974ae2889b5c1d8008ad4dabfb
mrtwenty <mrtwenty@foxmail.com>
2022-01-03 14:28:54 +0800

    flower client

---
+++ src/Client.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flower;
+
+/**
+ * 消息队列
+ */
+class Client
+{
+    protected $redis;
+    protected $mq;
+
+    /**
+     * @param mixed $redis 
+     * @param array $mq mq配置
+     */
+    public function __construct($redis, $mq)
+    {
+        $this->redis = $redis;
+        $this->mq = $mq; //name、delay_name、fail_list
+    }
+
+    /**
+     * 添加消息
+     * @param mixed $data 内容
+     * @param int   $delay 延迟秒数
+     * @return string|int
+     */
+    public function add($data, $delay = 0)
+    {
+        if ($delay == 0) {
+            $mq_name     = $this->mq['name']; //消息队列名
+            $package_str = \json_encode($data, JSON_UNESCAPED_UNICODE);
+            return $this->redis->xadd($mq_name, '*', ['data' => $package_str]);
+        }
+
+        $mq_delay_name = $this->mq['delay_name'];
+
+        $now = time();
+
+        //序列化
+        $msg = [
+            'id'   => random_int(PHP_INT_MIN, PHP_INT_MAX),
+            'time' => $now,
+            'data' => $data,
+        ];
+        $package_str = \json_encode($msg, JSON_UNESCAPED_UNICODE);
+        $id = $now + $delay;
+        if ($this->redis->zadd($mq_delay_name, $id, $package_str)) {
+            return $id;
+        }
+        return 0;
+    }
+
+    /**
+     * 确认消息
+     * @param string $group_name 分组名
+     * @param string $id
+     */
+    public function ack($group_name, $id)
+    {
+        $mq_name    = $this->mq['name']; //消息队列名
+        return $this->redis->xAck($mq_name, $group_name, [$id]);
+    }
+}
---
+++ test/client.php
@@ -0,0 +1,45 @@
+<?php
+
+use Flower\Client;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+function redis()
+{
+    $redis_host = '192.168.124.105';
+    $redis_port = 6379;
+    $redis_auth = '123456';
+    $redis_db   = 1;
+
+    $redis = new \Redis();
+    if ($redis->connect($redis_host, $redis_port) !== true) {
+        throw new \Exception("redis connect error", 1);
+    }
+
+    //密码
+    if ($redis_auth !== '' && $redis->auth($redis_auth) !== true) {
+        throw new \Exception("redis auth error", 1);
+    }
+
+    //测试链接
+    if (!$redis->ping()) {
+        throw new \Exception("redis connect error", 1);
+    }
+
+    //选择数据库
+    $redis->select($redis_db);
+    return $redis;
+}
+$redis = redis();
+$mq    = [
+    'name' => 'mq',
+    'delay_name' => 'mq_delay',
+    'fail_list' => 'mq_fail_list'
+];
+$client = new Client($redis, $mq);
+
+$res = $client->add(['test' => 'data']);
+var_dump($res);
+
+$res = $client->add(['test' => 'data'], 3);
+var_dump($res);
